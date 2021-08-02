b6a33464ebf2682475a69e39e7a314d8c8babeb2
lu <piece7775@hotmail.com.tw>
2021-08-03 01:02:13 +0800

    edited wooConfig

--- wooConfig.js
+++ wooConfig.js
@@ -2,7 +2,7 @@ const wooConfig ={
     siteUrl: 'http://localhost/wordpress',
     consumerKey: 'ck_1207152e8aa0791c83473f021db146a52434a875',
     consumerSecret: 'cs_c4121fe68b4fe74ce065a005de1477f268c98058',
-    graphqlUrl:'http://localhost/wordpress/graphql'
+    graphqlUrl:'https://www.thelivablestudio.com/graphql'
 };
 
 module.exports = wooConfig;
